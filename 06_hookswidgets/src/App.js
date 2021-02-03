c182f753f40c81a1686a092d3190b14ff4ef7b48
Apprentice For Life <sevketevli@gmail.com>
2021-02-03 23:12:48 +0300

    Add files via upload

---
+++ 06_hookswidgets/src/App.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Translate from "./components/Translate";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Route from "./components/Route";
+
+// Static Data
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end js framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite js library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
+// Route by func
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
+const showComponent = (route, component) => {
+  return window.location.pathname === route ? component : null;
+};
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
+
+export default App;
+
+/*
+  // Route can be made by using funcs
+  export default () => {
+  return (
+    <div>
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
+    </div>
+  );
+};
+
+*/
